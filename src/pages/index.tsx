879e981685ba60700b1e89b677fab41e1ac176a2
Zvrksam86 <zvrkatwork@gmail.com>
2021-10-27 21:25:23 +0200

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+	return (
+		<div className="bg-gray-400 text-3xl text-gray-900">Next starter!</div>
+	);
+};
+
+export default Home;
